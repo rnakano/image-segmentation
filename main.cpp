bc502b95b2573f0eecb57f80464a0bdd8807a3b5
NAKANO Ryusuke <rsk.nakano@gmail.com>
2016-01-09 15:03:18 +0900

    read pixels

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <iostream>
 #include <opencv2/opencv.hpp>
 
 int main(int argc, char *argv[])
 {
     cv::Mat image = cv::imread("data/text1.png");
-    printf("hello\n");
+
+    for (int j = 0; j < image.rows; j++){
+      for (int i = 0; i < image.cols; i++){
+        cv::Vec3b pixel = image.at<cv::Vec3b>(j, i);
+      }
+    }
+
     return 0;
 }
